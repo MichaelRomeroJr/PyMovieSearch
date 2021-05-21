b94ed94947c59f2de9c3352326ade7ba659b5335
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2021-05-21 15:10:37 -0700

    Update netflix.py

--- Netflix/netflix.py
+++ Netflix/netflix.py
@@ -3,32 +3,18 @@ import config
 from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains
 
-def account_login(driver):
+def login(driver):
+	driver.get("https://www.netflix.com/")
+	input("Manually sign into Netflix press Enter to continue: ")
+
+	return
+
+
+def select_profile(driver):
+	"""
+		Set Netflix profile name in the config file as NETFLIX_ACC
+	"""
 	
-	# Landing on log in page, if you are already signed in it re-directs to /browse page
-	if "browse" not in driver.current_url:
-		# Click "Sign In" button
-		try: 
-			sign_in_button = driver.find_element_by_partial_link_text('Sign In')
-			sign_in_button.click()
-			sleep(3)
-
-			# Send email/pass
-			username = config.NETFLIX_EMAIL
-			email_field = driver.find_element_by_name("userLoginId")
-			email_field.send_keys(username)
-
-			password = config.NETFLIX_PASS
-			password_field = driver.find_element_by_name("password")
-			password_field.send_keys(password)
-			sleep(1)
-
-			# Click to log in
-			sign_in_button = driver.find_element_by_xpath("//button[@class='btn login-button btn-submit btn-small']")
-			sign_in_button.click()
-			sleep(3)
-		except:
-			input(f"failed at Netflix log in")
 	try:
 		# Select profile
 		profiles = driver.find_elements_by_class_name("profile-name")
@@ -36,10 +22,12 @@ def account_login(driver):
 			#print(profile.text) # print names of Netflix account names
 			if config.NETFLIX_ACC == profile.text:
 				profile.click()
+				return
 	except:
-		# Prompt to select Netflix account isn't there 
-		pass
+		print(f"select_profile fail")
+		
 	return
+
 
 def movie_search(driver, movie_title):
 	driver.find_element_by_class_name("icon-search").click()
@@ -54,6 +42,7 @@ def movie_search(driver, movie_title):
 	sleep(2) 
 	return
 
+
 def scan_results(driver, movie_title):
 
 	movie_found = False
@@ -66,10 +55,11 @@ def scan_results(driver, movie_title):
 
 	return movie_found
 
+
 def search(driver, movie_title):
 	driver.get("https://www.netflix.com/")
 
-	account_login(driver)
+	select_profile(driver)
 	movie_search(driver, movie_title=movie_title)
 	
 	is_on_netflix = scan_results(driver, movie_title=movie_title)
