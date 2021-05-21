27a29f73860ec4853650cf9a6922cf7882e9bba5
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2021-05-21 15:11:28 -0700

    Update disneyplus.py

--- DisneyPlus/disneyplus.py
+++ DisneyPlus/disneyplus.py
@@ -3,18 +3,15 @@ import config
 from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains
 
-def account_login(driver):
-	sign_in_button = driver.find_elements_by_class_name("btn-secondary")
-
-	for element in sign_in_button:
-		#print(element.text)
-		if "LOG IN" in element.text:
-			input("First time Disney+ log in: ")
-			return
+def login(driver):
+	
+	driver.get("https://www.disneyplus.com/home")
+	input("Manually sign into Disney+ press Enter to continue: ")
 
 	return
 
 def movie_search(driver, movie_title):
+
 	driver.get("https://www.disneyplus.com/search")
 	sleep(2)
 
@@ -44,16 +41,16 @@ def scan_results(driver, movie_title):
 				for e in sub_elements:
 					current_title = e.get_attribute('aria-label')
 					if movie_title in current_title:
-						print(f"Found {movie_title} as {current_title}")
+						print(f"Found '{movie_title}' as '{current_title}' ")
 						movie_found = True
 
 	return movie_found
 
 def search(driver, movie_title):
+	
 	driver.get("https://www.disneyplus.com/home")
 	sleep(5)
 
-	account_login(driver)
 	movie_search(driver, movie_title=movie_title)
 	
 	is_on_disneyplus = scan_results(driver, movie_title=movie_title)
